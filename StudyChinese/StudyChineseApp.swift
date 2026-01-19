c2c1d6a156a4f4e32f23085eaee934beaaf6e3b2
刘耀文 <287228558@qq.com>
2026-01-19 09:23:53 +0800

    初始化

---
+++ StudyChinese/StudyChineseApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyChineseApp.swift
+//  StudyChinese
+//
+//  Created by 刘耀文 on 2026/1/19.
+//
+
+import SwiftUI
+
+@main
+struct StudyChineseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
